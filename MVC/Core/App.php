aee782513b3ada2381db8c93296ac3a11b15c067
Nguyễn Quang Nghị <nghinq28@gmail.com>
2021-04-04 20:17:54 +0700

    Update App.php

--- MVC/Core/App.php
+++ MVC/Core/App.php
@@ -11,6 +11,7 @@
             if(isset($arr[0])){
                 if(file_exists("./mvc/controller/".$arr[0].".php")){
                     $this->controller = $arr[0];
+                    unset($arr[0]);
                 } 
             }
         
@@ -21,9 +22,13 @@
                 if(method_exists($this->controller,$arr[1])){
                     $this->action = $arr[1];
                 }
+                unset($arr[1]);
             }
+             $this->params = $arr?array_values($arr):[];
+            print_r($this->params);
+
         }
-             
+            
         function getUrl()
         {
            if(isset($_GET["url"])){
